b26e9f0111dd1d4c1e06b5cbd469c9940f0a3564
johnvesslyalti <altijohnvessly@gmail.com>
2025-06-24 14:28:18 +0530

    refactor: restructure authentication options to directly include Google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,12 @@
-import { authOptions } from "@/lib/auth";
-import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
